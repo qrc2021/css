df96f50661d29654369b799841e6914c17604d3a
qrc2021 <q.crisostomo@knights.ucf.edu>
2022-03-15 16:28:24 -0400

    following poop tutorial

---
+++ frontend/src/components/PageHeader.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function PageHeader()
+{
+    return(
+        <h1 id="title">CSS BAYBAY</h1>
+    );
+};
+
+export default PageHeader;
---
+++ frontend/src/pages/LoginPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import PageHeader from '../components/PageHeader';
+import Login from '../components/Login';
+
+const LoginPage = () =>
+{
+    return(
+        <div>
+            <PageHeader />
+            <Login />
+        </div>
+    );
+};
+
+export default LoginPage;
