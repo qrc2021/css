50f035ea9c261a7b944b2dec7ce4a84ad79f10c4
qrc2021 <q.crisostomo@knights.ucf.edu>
2022-03-17 23:18:19 -0400

    routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from 'react-router-dom';
 import './App.css';
 
 import LoginPage from './pages/LoginPage';
@@ -7,8 +13,17 @@ import CssPage from './pages/CssPage';
 function App()
 {
   return (
-    // <LoginPage />
-    <CssPage />
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <LoginPage />
+        </Route>
+        <Route path="/css" exact>
+          <CssPage />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+    </Router>
   );
 };
 
