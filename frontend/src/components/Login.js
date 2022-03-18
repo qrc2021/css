9143b12e4e3457f32de40ca884438d424729dc4f
qrc2021 <q.crisostomo@knights.ucf.edu>
2022-03-18 10:34:16 -0400

    IFP for login

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,20 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function Login()
 {
+    var loginName; // input field persistence
+    var loginPassword; 
+    const [message, setMessage] = useState('');
+
     const doLogin = async event =>
     {
         event.preventDefault();
 
-        alert('doIt()');
+        alert('doIt()' + loginName.value + ' ' + loginPassword.value);
     };
 
     return(
         <div id="loginDiv">
             <form onSubmit={doLogin}>
-                <span id="inner-title">Login yo</span><br />
-                <input type="text" id="loginName" placeholder="Username" /><br />
-                <input type="text" id="loginPassword" placeholder="Password" /><br />
+                <span id="loginResult">{message}</span><br />
+                <input type="text" id="loginName" placeholder="Username" ref={(c) => loginName = c}/><br />
+                <input type="text" id="loginPassword" placeholder="Password" ref={(c) => loginPassword = c}/><br />
                 <input type="submit" id="loginButton" class="buttons" value = "Do it" onClick={doLogin} />
             </form>
             <span id="loginResult"></span>
