38e04abafa04993f583e0d794af13c3fb208175a
qrc2021 <q.crisostomo@knights.ucf.edu>
2022-03-18 16:05:08 -0400

    making password type password

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -18,7 +18,7 @@ function Login()
             <form onSubmit={doLogin}>
                 <span id="loginResult">{message}</span><br />
                 <input type="text" id="loginName" placeholder="Username" ref={(c) => loginName = c}/><br />
-                <input type="text" id="loginPassword" placeholder="Password" ref={(c) => loginPassword = c}/><br />
+                <input type="password" id="loginPassword" placeholder="Password" ref={(c) => loginPassword = c}/><br />
                 <input type="submit" id="loginButton" class="buttons" value = "Do it" onClick={doLogin} />
             </form>
             <span id="loginResult"></span>
