65b0b57e39f0dddf8c6b168f7884f632b1088b8c
qrc2021 <q.crisostomo@knights.ucf.edu>
2022-03-19 00:19:50 -0400

    bye

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,29 +11,31 @@ import LoginPage from './pages/LoginPage';
 import CssPage from './pages/CssPage';
 import RegisterPage from './pages/RegisterPage';
 import ForgotPage from './pages/ForgotPage';
+import Register from './components/Register';
 
 
 
 function App()
 {
   return (
-    <AuthProvider>
+    
       <Container 
         className="d-flex align-items-center justify-content-center"
         styple={{ minHeight: "100vh" }}
         >
         <div className="w-100" style={{ maxWidth: "400px" }}>
-          <Router>
+          {/* <Router>
             <Routes>
               <Route exact path="/" element={<LoginPage />} />
               <Route exact path="/car" element={<CssPage />} />
               <Route exact path="/register" element={<RegisterPage />} />
               <Route exact path="/forgot" element={<ForgotPage />} />
             </Routes>
-          </Router>
+          </Router> */}
+          <Register />
         </div>
       </Container>
-    </AuthProvider>
+    
   );
 };
 
