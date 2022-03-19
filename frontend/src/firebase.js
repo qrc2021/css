5563c7604a83e6d44e25eac040bd09dd28d6bdc9
qrc2021 <q.crisostomo@knights.ucf.edu>
2022-03-18 22:20:54 -0400

    configuring fb

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,24 +1,15 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getAuth } from 'firebase/auth';
+import firebase from "firebase/app"
+import "firebase/auth"
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+const app = firebase.initializeApp({
+  apiKey: process.env.REACT_APP_FB_apiKey,
+  authDomain: process.env.REACT_APP_FB_authDomain,
+  projectId: process.env.REACT_APP_FB_projectId,
+  storageBucket: process.env.REACT_APP_FB_storageBucket,
+  messagingSenderId: process.env.REACT_APP_FB_messagingSenderId,
+  appId: process.env.REACT_APP_FB_appId,
+  measurementId: process.env.REACT_APP_FB_measurementId
+})
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyD4ONFOoJsJQhy2c7c6tibUvE3448BTJ28",
-  authDomain: "css---car-sentry-system.firebaseapp.com",
-  projectId: "css---car-sentry-system",
-  storageBucket: "css---car-sentry-system.appspot.com",
-  messagingSenderId: "235726709305",
-  appId: "1:235726709305:web:52c66c7f37ce269b6e23e8",
-  measurementId: "G-MC5WRY5BRK"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-export const auth = getAuth(app); // so we can import it in different files
+export const auth = app.auth()
+export default app
